8880e15351bb4dd220cb626e5271c23ad34f5f78
Matt kikuchi <matt.kikuchi@tripstack.com>
2021-01-29 01:51:14 -0500

    Cleanup Day07 - Setup Day 08

---
+++ src/2020/day08/part1.js
@@ -0,0 +1,8 @@
+// --- Day 8: Report Repair Part 1 ---
+// https://adventofcode.com/2020/day/8
+// Part1.js
+// Matt Kikuchi (https://github.com/k1kuma)
+// ----------------------------------------
+
+var fs = require("fs");
+var text = fs.readFileSync("./input.txt", "utf-8");
---
+++ src/2020/day08/part2.js
@@ -0,0 +1,8 @@
+// --- Day 7: Report Repair Part 2 ---
+// https://adventofcode.com/2020/day/7
+// Part2.js
+// Matt Kikuchi (https://github.com/k1kuma)
+// ----------------------------------------
+
+var fs = require("fs");
+var text = fs.readFileSync("./input.txt", "utf-8");
